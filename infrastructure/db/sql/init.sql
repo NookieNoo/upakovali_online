3e5adaa5a73aca613908843b2aedddce5a634ae0
NookieNoo <biv0208@yandex.ru>
2021-09-18 23:47:43 +0300

    Завели постгрес и пхп8 раздельно

---
+++ infrastructure/db/sql/init.sql
@@ -0,0 +1,4 @@
+CREATE DATABASE IF NOT EXISTS `db_dev` default character set utf8 collate utf8_general_ci;
+CREATE DATABASE IF NOT EXISTS `db_test` default character set utf8 collate utf8_general_ci;
+
+USE db_dev;
