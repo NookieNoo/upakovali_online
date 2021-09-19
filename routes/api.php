547d71f3dbaebe1f4926a8e74ccd0900e94bd34f
NookieNoo <biv0208@yandex.ru>
2021-09-19 22:27:25 +0300

    add ide_helper, client/parthner controllers

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\ClientController;
+use App\Http\Controllers\ParthnerController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +16,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+//Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//    return $request->user();
+//});
+Route::get('/client', [ClientController::class, 'index']);
+Route::get('/parthner', [ParthnerController::class, 'index']);
