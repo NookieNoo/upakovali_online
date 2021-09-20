e31cc77079bded74c71fef6770ba6df0562662b4
NookieNoo <biv0208@yandex.ru>
2021-09-20 21:53:15 +0300

    Заглушка для афторизации на фронте

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,11 +2,15 @@ import * as React from "react";
 import { Admin, Resource, ListGuesser } from "react-admin";
 import jsonServerProvider from "ra-data-json-server";
 import Dashboard from "@app-pages/Dashboard";
-
+import authProvider from "./authProvider";
 
 const dataProvider = jsonServerProvider("https://jsonplaceholder.typicode.com");
 const App = () => (
-    <Admin dataProvider={dataProvider} dashboard={Dashboard}>
+    <Admin
+        dataProvider={dataProvider}
+        dashboard={Dashboard}
+        authProvider={authProvider}
+    >
         <Resource name="users" list={ListGuesser} />
     </Admin>
 );
