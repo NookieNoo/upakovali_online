d9f6ecc3d0eb35db34237dd44087d24214808d87
NookieNoo <biv0208@yandex.ru>
2021-10-17 18:45:18 +0300

    Верстка дешборда

--- frontend/src/components/pages/Dashboard/Dashboard.js
+++ frontend/src/components/pages/Dashboard/Dashboard.js
@@ -1,145 +1,90 @@
 import * as React from 'react';
 import { MapWithOrders } from './blocks';
 import { OrdersList } from './blocks';
-import { Card, CardHeader, CardContent, Grid, useMediaQuery } from '@material-ui/core';
+import { Card, CardHeader, CardContent, Grid, Box, Container } from '@material-ui/core';
 import { Link } from 'react-router-dom';
-import Chip from '@material-ui/core/Chip';
 import Button from '@material-ui/core/Button';
+import { ShowSplitter } from '@app-universal';
 
-export const Dashboard = () => {
-    const isXSmall = useMediaQuery((theme) => theme.breakpoints.down('xs'));
-    const isSmall = useMediaQuery((theme) => theme.breakpoints.down('md'));
-    console.log('isXSmall', isXSmall);
-    console.log('isSmall', isSmall);
-    return isXSmall ? (
-        <Grid container>
-            <Grid item xs={12}>
-                <Card style={{ width: '100%' }}>
-                    <CardHeader title="Найти" />
-                    <CardContent>
-                        <Button size="small" color="primary" component={Link} to={{ pathname: '/user' }}>
-                            Пользователя
-                        </Button>
-                        <Button size="small" color="primary" component={Link} to={{ pathname: '/parthner' }}>
-                            Партнера
-                        </Button>
-                        <Button size="small" color="primary" component={Link} to={{ pathname: '/client' }}>
-                            Клиента
-                        </Button>
-                    </CardContent>
-                </Card>
-            </Grid>
-
-            <Grid item xs={12}>
-                <Card style={{ width: '100%' }}>
-                    <CardHeader title="Создать" />
-                    <CardContent>
-                        <Button size="small" color="primary" component={Link} to={{ pathname: '/user/create' }}>
-                            Пользователя
-                        </Button>
-                        <Button size="small" color="primary" component={Link} to={{ pathname: '/parthner/create' }}>
-                            Партнера
-                        </Button>
-                        <Button size="small" color="primary" component={Link} to={{ pathname: '/client/create' }}>
-                            Клиента
-                        </Button>
-                    </CardContent>
-                </Card>
-            </Grid>
-
-            <Grid item xs={12} sm={6} md={4}>
-                <MapWithOrders />
-            </Grid>
-            <Grid item xs={12} sm={6} md={4}>
-                <OrdersList />
-            </Grid>
-        </Grid>
-    ) : isSmall ? (
-        <Grid container>
-            <Grid item md={6}>
-                <Card style={{ width: '100%' }}>
-                    <CardHeader title="Найти" />
-                    <CardContent>
-                        <Button size="small" color="primary" component={Link} to={{ pathname: '/user' }}>
-                            Пользователя
-                        </Button>
-                        <Button size="small" color="primary" component={Link} to={{ pathname: '/parthner' }}>
-                            Партнера
-                        </Button>
-                        <Button size="small" color="primary" component={Link} to={{ pathname: '/client' }}>
-                            Клиента
-                        </Button>
-                    </CardContent>
-                </Card>
-            </Grid>
-
-            <Grid item md={6}>
-                <Card style={{ width: '100%' }}>
-                    <CardHeader title="Создать" />
-                    <CardContent>
-                        <Button size="small" color="primary" component={Link} to={{ pathname: '/user/create' }}>
-                            Пользователя
-                        </Button>
-                        <Button size="small" color="primary" component={Link} to={{ pathname: '/parthner/create' }}>
-                            Партнера
-                        </Button>
-                        <Button size="small" color="primary" component={Link} to={{ pathname: '/client/create' }}>
-                            Клиента
-                        </Button>
-                    </CardContent>
-                </Card>
-            </Grid>
-
-            <Grid item md={12}>
-                <MapWithOrders />
-            </Grid>
-            <Grid item md={12}>
-                <OrdersList />
-            </Grid>
-        </Grid>
-    ) : (
-        <Grid container>
-            <Grid item md={6}>
-                <Grid container>
-                    <Grid item md={6}>
-                        <Card>
-                            <CardHeader title="Найти" />
-                            <CardContent>
-                                <Button size="small" color="primary" component={Link} to={{ pathname: '/user' }}>
-                                    Пользователя
-                                </Button>
-                                <Button size="small" color="primary" component={Link} to={{ pathname: '/parthner' }}>
-                                    Партнера
-                                </Button>
-                                <Button size="small" color="primary" component={Link} to={{ pathname: '/client' }}>
-                                    Клиента
-                                </Button>
-                            </CardContent>
-                        </Card>
+export const Dashboard = (props) => {
+    return (
+        <div {...props}>
+            <ShowSplitter
+                leftSide={
+                    <Grid container spacing={2}>
+                        <Container>
+                            <Grid container spacing={1}>
+                                <Grid item lg={6} md={6} xl={6} xs={6}>
+                                    <Card>
+                                        <CardHeader title="Найти" />
+                                        <CardContent>
+                                            <Button
+                                                size="small"
+                                                color="primary"
+                                                component={Link}
+                                                to={{ pathname: '/user' }}
+                                            >
+                                                Пользователя
+                                            </Button>
+                                            <Button
+                                                size="small"
+                                                color="primary"
+                                                component={Link}
+                                                to={{ pathname: '/parthner' }}
+                                            >
+                                                Партнера
+                                            </Button>
+                                            <Button
+                                                size="small"
+                                                color="primary"
+                                                component={Link}
+                                                to={{ pathname: '/client' }}
+                                            >
+                                                Клиента
+                                            </Button>
+                                        </CardContent>
+                                    </Card>
+                                </Grid>
+                                <Grid item lg={6} md={6} xl={6} xs={6}>
+                                    <Card>
+                                        <CardHeader title="Создать" />
+                                        <CardContent>
+                                            <Button
+                                                size="small"
+                                                color="primary"
+                                                component={Link}
+                                                to={{ pathname: '/user/create' }}
+                                            >
+                                                Пользователя
+                                            </Button>
+                                            <Button
+                                                size="small"
+                                                color="primary"
+                                                component={Link}
+                                                to={{ pathname: '/parthner/create' }}
+                                            >
+                                                Партнера
+                                            </Button>
+                                            <Button
+                                                size="small"
+                                                color="primary"
+                                                component={Link}
+                                                to={{ pathname: '/client/create' }}
+                                            >
+                                                Клиента
+                                            </Button>
+                                        </CardContent>
+                                    </Card>
+                                </Grid>
+                            </Grid>
+                            <MapWithOrders />
+                        </Container>
                     </Grid>
-                    <Grid item md={6}>
-                        <Card>
-                            <CardHeader title="Создать" />
-                            <CardContent>
-                                <Button size="small" color="primary" component={Link} to={{ pathname: '/user/create' }}>
-                                    Пользователя
-                                </Button>
-                                <Button size="small" color="primary" component={Link} to={{ pathname: '/parthner/create' }}>
-                                    Партнера
-                                </Button>
-                                <Button size="small" color="primary" component={Link} to={{ pathname: '/client/create' }}>
-                                    Клиента
-                                </Button>
-                            </CardContent>
-                        </Card>
-                    </Grid>
-                </Grid>
-
-                <MapWithOrders />
-            </Grid>
-            <Grid item md={6}>
-                <OrdersList />
-            </Grid>
-        </Grid>
+                }
+                leftSideProps={{ lg: 6, md: 6, xl: 6, xs: 12, component: Box }}
+                rightSideProps={{ lg: 6, md: 6, xl: 6, xs: 12 }}
+                rightSide={<OrdersList />}
+            />
+        </div>
     );
 };
