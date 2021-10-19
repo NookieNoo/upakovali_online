3c0aeea828887b70cb446c7b3a936b2199761794
NookieNoo <biv0208@yandex.ru>
2021-10-19 22:53:17 +0300

    fux

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,5 @@
 import * as React from 'react';
 import { Admin, Resource } from 'react-admin';
-import { ResourceWithPermission } from 'ra-access-control-lists';
 import { createBrowserHistory as createHistory } from 'history';
 import {
     Dashboard,
@@ -9,23 +8,20 @@ import {
     OrderList,
     UserList,
     ClientEdit,
-    ClientIcon,
     ClientShow,
     ClientCreate,
     OrderEdit,
-    OrderIcon,
     OrderShow,
     OrderCreate,
-    ParthnerIcon,
     ParthnerEdit,
     ParthnerShow,
     ParthnerCreate,
-    UserIcon,
     UserEdit,
     UserShow,
     UserCreate,
 } from '@app-pages';
 import { dataProvider, i18nProvider, authProvider } from '@app-providers';
+import { ResourceWithPermissions, CustomLayout } from '@app-components/overriding';
 
 const history = createHistory();
 const options = {
@@ -51,43 +47,42 @@ const App = () => (
         dashboard={Dashboard}
         authProvider={authProvider}
         i18nProvider={i18nProvider}
+        layout={CustomLayout}
+        title="upakovali.online"
     >
-        <ResourceWithPermission
+        <ResourceWithPermissions
             name="client"
             list={ClientList}
             edit={ClientEdit}
-            icon={ClientIcon}
             show={ClientShow}
             create={ClientCreate}
             options={options.client}
         />
-        <ResourceWithPermission
+        <ResourceWithPermissions
             name="parthner"
             list={ParthnerList}
-            icon={ParthnerIcon}
             edit={ParthnerEdit}
             show={ParthnerShow}
             create={ParthnerCreate}
             options={options.parthner}
         />
-        <ResourceWithPermission
+        <ResourceWithPermissions
             name="order"
             list={OrderList}
             edit={OrderEdit}
-            icon={OrderIcon}
             show={OrderShow}
             create={OrderCreate}
             options={options.order}
         />
-        <ResourceWithPermission
+        <ResourceWithPermissions
             name="user"
             list={UserList}
-            icon={UserIcon}
             edit={UserEdit}
             show={UserShow}
             create={UserCreate}
             options={options.user}
         />
+
         <Resource name="source" />
         <Resource name="workshop" />
         <Resource name="addressee" />
