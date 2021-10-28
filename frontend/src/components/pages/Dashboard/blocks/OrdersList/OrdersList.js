6eab1ce0ec5539a0b0793ace4fd899d028fbc62e
NookieNoo <biv0208@yandex.ru>
2021-10-28 23:21:19 +0300

    Вывод реальных заказов в дэшборде

--- frontend/src/components/pages/Dashboard/blocks/OrdersList/OrdersList.js
+++ frontend/src/components/pages/Dashboard/blocks/OrdersList/OrdersList.js
@@ -8,33 +8,40 @@ import {
     ListItemText,
     ListItemSecondaryAction,
     Avatar,
+    Typography,
 } from '@material-ui/core';
+import { List as RaList, ListBase, SimpleList, useListContext } from 'react-admin';
 
-const orderList = [
-    { p: 'Ozon', c: 'Mitlabs', date1: '10/09/2021, 12:53:18', price: '500$' },
-    { p: 'Ozon', c: 'Mitlabs', date1: '10/09/2021, 12:53:18', price: '500$' },
-    { p: 'Ozon', c: 'Mitlabs', date1: '10/09/2021, 12:53:18', price: '500$' },
-    { p: 'Ozon', c: 'Mitlabs', date1: '10/09/2021, 12:53:18', price: '500$' },
-    { p: 'Ozon', c: 'Mitlabs', date1: '10/09/2021, 12:53:18', price: '500$' },
-    { p: 'Ozon', c: 'Mitlabs', date1: '10/09/2021, 12:53:18', price: '500$' },
-    { p: 'Ozon', c: 'Mitlabs', date1: '10/09/2021, 12:53:18', price: '500$' },
-];
+const postRowStyle = (record, index) => ({
+    backgroundColor: record.nb_views >= 500 ? '#efe' : 'white',
+});
 
-export function OrdersList() {
+export function OrdersList(props) {
+    const { basePath } = useListContext();
     return (
-        <Card style={{ width: '100%' }}>
-            <CardHeader title="Список заказов" />
-            <List dense={true}>
-                {orderList.map((it, index) => (
-                    <ListItem key={index}>
-                        <ListItemAvatar>
-                            <Avatar />
-                        </ListItemAvatar>
-                        <ListItemText primary={it.date1} secondary={it.p + ' ' + it.c} />
-                        <ListItemSecondaryAction>{it.price}</ListItemSecondaryAction>
-                    </ListItem>
-                ))}
-            </List>
-        </Card>
+        // <Card style={{ width: '100%' }}>
+        //     <CardHeader title="Список заказов" />
+        //     <List dense={true}>
+        //         {orderList.map((it, index) => (
+        //             <ListItem key={index}>
+        //                 <ListItemAvatar>
+        //                     <Avatar />
+        //                 </ListItemAvatar>
+        //                 <ListItemText primary={it.date1} secondary={it.p + ' ' + it.c} />
+        //                 <ListItemSecondaryAction>{it.price}</ListItemSecondaryAction>
+        //             </ListItem>
+        //         ))}
+        //     </List>
+        // </Card>
+        <ListBase basePath={basePath} resource="order">
+            <CardHeader title="Список заказов на сегодня" />
+            <SimpleList
+                primaryText={(record) => 'Клиент: ' + record.client.full_name}
+                secondaryText={(record) => 'Мастер: ' + record.master.full_name}
+                tertiaryText={(record) => record.issue_date}
+                linkType={false}
+                rowStyle={postRowStyle}
+            />
+        </ListBase>
     );
 }
