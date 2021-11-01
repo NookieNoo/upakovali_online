88c58c5eb5d80dfb72fa994c16d1b886eae85800
NookieNoo <biv0208@yandex.ru>
2021-11-01 22:25:18 +0300

    parthner autocomplete

--- frontend/src/components/pages/Parthner/ParthnerCreate/ParthnerCreate.js
+++ frontend/src/components/pages/Parthner/ParthnerCreate/ParthnerCreate.js
@@ -1,7 +1,8 @@
 import * as React from 'react';
-import { Create, SimpleForm, TextInput, ReferenceInput, SelectInput } from 'react-admin';
+import { Create, SimpleForm, TextInput } from 'react-admin';
 import { createParthnerFormValidators } from '@app-helpers';
 import { userRoles } from '@app-constants';
+import { AutocompleteWithRef } from '@app-universal';
 
 const managerFilter = { role_id: userRoles.manager.id };
 
@@ -13,9 +14,13 @@ export default function ParthnerCreate(props) {
                 <TextInput label="Email" source="email" validate={createParthnerFormValidators.email} />
                 <TextInput label="Телефон" source="phone" validate={createParthnerFormValidators.phone} />
                 <TextInput label="Комментарий" source="comment" validate={createParthnerFormValidators.comment} />
-                <ReferenceInput label="Менеджер" source="manager_id" reference="user" filter={managerFilter} validate={createParthnerFormValidators.manager_id}>
-                    <SelectInput optionText="full_name" optionValue="id" />
-                </ReferenceInput>
+                <AutocompleteWithRef
+                    label="Менеджер"
+                    source="manager_id"
+                    reference="user"
+                    filter={managerFilter}
+                    validate={createParthnerFormValidators.manager_id}
+                />
             </SimpleForm>
         </Create>
     );
--- frontend/src/components/pages/Parthner/ParthnerEdit/ParthnerEdit.js
+++ frontend/src/components/pages/Parthner/ParthnerEdit/ParthnerEdit.js
@@ -1,21 +1,26 @@
 import * as React from 'react';
-import { Edit, SimpleForm, TextInput, ReferenceInput, SelectInput } from 'react-admin';
+import { Edit, SimpleForm, TextInput } from 'react-admin';
 import { editParthnerFormValidators } from '@app-helpers';
 import { userRoles } from '@app-constants';
+import { AutocompleteWithRef } from '@app-universal';
 
 const managerFilter = { role_id: userRoles.manager.id };
 
 export default function ParthnerEdit(props) {
     return (
-        <Edit {...props} mutationMode='pessimistic'>
+        <Edit {...props} mutationMode="pessimistic">
             <SimpleForm>
                 <TextInput label="ФИО" source="full_name" validate={editParthnerFormValidators.full_name} />
                 <TextInput label="Email" source="email" validate={editParthnerFormValidators.email} />
                 <TextInput label="Телефон" source="phone" validate={editParthnerFormValidators.phone} />
                 <TextInput label="Комментарий" source="comment" validate={editParthnerFormValidators.comment} />
-                <ReferenceInput label="Менеджер" source="manager_id" reference="user" filter={managerFilter}>
-                    <SelectInput optionText="full_name" optionValue="id" validate={editParthnerFormValidators.manager_id} />
-                </ReferenceInput>
+                <AutocompleteWithRef
+                    label="Менеджер"
+                    source="manager_id"
+                    reference="user"
+                    filter={managerFilter}
+                    validate={editParthnerFormValidators.manager_id}
+                />
             </SimpleForm>
         </Edit>
     );
