f3c5950382829066c25bf12d129d78dd05db5cd7
NookieNoo <biv0208@yandex.ru>
2021-11-10 02:03:11 +0300

    test https swagger

--- resources/views/vendor/l5-swagger/index.blade.php
+++ resources/views/vendor/l5-swagger/index.blade.php
@@ -8,6 +8,7 @@
   <link rel="stylesheet" type="text/css" href="{{ l5_swagger_asset($documentation, 'swagger-ui.css') }}" >
   <link rel="icon" type="image/png" href="{{ l5_swagger_asset($documentation, 'favicon-32x32.png') }}" sizes="32x32" />
   <link rel="icon" type="image/png" href="{{ l5_swagger_asset($documentation, 'favicon-16x16.png') }}" sizes="16x16" />
+  <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
   <style>
     html
     {
