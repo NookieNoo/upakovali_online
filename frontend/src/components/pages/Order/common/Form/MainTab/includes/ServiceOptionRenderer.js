b5435a7b38854ccdec4afd4649368af28d4d260c
NookieNoo <biv0208@yandex.ru>
2022-04-27 18:55:00 +0300

    refactor: div -> Box

--- frontend/src/components/pages/Order/common/Form/MainTab/includes/ServiceOptionRenderer.js
+++ frontend/src/components/pages/Order/common/Form/MainTab/includes/ServiceOptionRenderer.js
@@ -1,50 +1,26 @@
 import * as React from 'react';
 import PropTypes from 'prop-types';
 import dayjs from 'dayjs';
+import { Box } from '@material-ui/core';
 
-const styles = {
-    div: {
-        width: '100%',
-    },
-    flex: {
-        display: 'flex',
-        flexDirection: 'column',
-        justifyContent: 'space-between',
-        alignItems: 'space-between',
-    },
-    firstStr: {
-        display: 'flex',
-        flexDirection: 'row',
-        fontSize: '0.9rem',
-        justifyContent: 'space-between',
-        alignItems: 'space-between',
-    },
-    secondStr: {
-        fontSize: '0.7rem',
-        display: 'flex',
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        alignItems: 'space-between',
-    },
-};
 const formatDate = (date) => dayjs(date).format('DD-MM-YYYY');
 const ServiceOptionRenderer = ({ id, name, sum, price }) => {
     return (
-        <div key={id} style={styles.div}>
-            <div style={styles.flex}>
-                <div style={styles.firstStr}>
+        <Box key={id} width="100%">
+            <Box display="flex" flexDirection="column">
+                <Box display="flex" justifyContent="space-between" fontSize="0.9rem">
                     <div>{name}</div>
                     <div>{sum}</div>
-                </div>
-                <div style={styles.secondStr}>
+                </Box>
+                <Box display="flex" justifyContent="space-between" fontSize="0.7rem">
                     <div>{price.name}</div>
-                    <div style={{ display: 'flex', flexDirection: 'column', lineHeight: 1 }}>
+                    <Box display="flex" flexDirection="column" style={{ lineHeight: 1 }}>
                         <div>{formatDate(price.start)}</div>
                         <div>{formatDate(price.end)}</div>
-                    </div>
-                </div>
-            </div>
-        </div>
+                    </Box>
+                </Box>
+            </Box>
+        </Box>
     );
 };
 
