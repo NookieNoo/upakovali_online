26353a67fe6514238f59453a2aa0e43b058933f1
Andrey <ver.ver.sus.sus@mail.ru>
2022-08-15 13:14:45 +0300

    Добавлена адаптация для клиентов/партнеров/прайсов

--- frontend/src/components/pages/Parthner/ParthnerList/ParthnerList.js
+++ frontend/src/components/pages/Parthner/ParthnerList/ParthnerList.js
@@ -1,6 +1,7 @@
 import * as React from 'react';
-import { List, TextField, EmailField, TextInput, SelectInput, ReferenceInput } from 'react-admin';
+import { List, TextField, EmailField, TextInput, SelectInput, ReferenceInput, SimpleList } from 'react-admin';
 import CustomizableDatagrid from 'components/overriding/ra-customizable-datagrid';
+import { useMediaQuery } from '@material-ui/core';
 
 const parthnerFilters = [
     <TextInput label="Название" source="query" alwaysOn />,
@@ -8,16 +9,32 @@ const parthnerFilters = [
 ];
 
 export default function ParthnerList(props) {
+    const isXSmall = useMediaQuery((theme) => theme.breakpoints.down('xs'));
+
     return (
         <List {...props} title="Партнеры" filters={parthnerFilters} bulkActionButtons={false}>
-            <CustomizableDatagrid rowClick="show" isRowSelectable={() => false}>
-                <TextField label="id" source="id" />
-                <TextField label="Название" source="full_name" />
-                <TextField label="Телефон" source="phone" />
-                <TextField label="Менеджер" source="manager" />
-                <EmailField label="Email" source="email" />
-                <TextField label="Комментарий" source="comment" />
-            </CustomizableDatagrid>
+            {isXSmall ? (
+                <SimpleList
+                    primaryText={(record) => `[${record.id}] ${record.full_name}`}
+                    secondaryText={(record) => 
+                        `Менеджер: ${record.manager}\n` +
+                        `Телефон: ${record.phone}\n` +
+                        `Почта: ${record.email}\n` +
+                        `Комментарий: ${record.comment}`
+                    }   
+                    rowStyle={() => ({ whiteSpace: 'pre-line'})}
+                    linkType="show" 
+                />
+            ) : (
+                <CustomizableDatagrid rowClick="show" isRowSelectable={() => false}>
+                    <TextField label="id" source="id" />
+                    <TextField label="Название" source="full_name" />
+                    <TextField label="Телефон" source="phone" />
+                    <TextField label="Менеджер" source="manager" />
+                    <EmailField label="Email" source="email" />
+                    <TextField label="Комментарий" source="comment" />
+                </CustomizableDatagrid>
+            )}
         </List>
     );
 }
--- frontend/src/components/pages/Price/PriceList/PriceList.js
+++ frontend/src/components/pages/Price/PriceList/PriceList.js
@@ -1,7 +1,8 @@
 import * as React from 'react';
-import { List, DateField, TextField, TextInput, DateInput, EditButton, ReferenceInput, SelectInput } from 'react-admin';
+import { List, DateField, TextField, TextInput, SimpleList, DateInput, EditButton, ReferenceInput, SelectInput } from 'react-admin';
 import CustomizableDatagrid from 'components/overriding/ra-customizable-datagrid';
 import { useHasAccess } from '@app-hooks';
+import { useMediaQuery } from '@material-ui/core';
 
 const defaultColumns = ['id', 'name', 'start', 'end', 'parthner.full_name'];
 
@@ -19,18 +20,33 @@ const priceFilters = [
 export default function PriceList(props) {
     const { resource } = props;
     const { edit: canEdit } = useHasAccess(resource);
+    const isXSmall = useMediaQuery((theme) => theme.breakpoints.down('xs'));
 
     return (
-        // <List {...props} title="Заказы" filters={filters}>
+        // <List {...props} title="Заказы" filters={filters}
         <List {...props} title="Прайсы" filters={priceFilters} bulkActionButtons={false}>
-            <CustomizableDatagrid rowClick="show" defaultColumns={defaultColumns} isRowSelectable={() => false}>
-                <TextField label="id" source="id" />
-                <TextField label="Партнер" source="parthner.full_name" />
-                <TextField label="Название" source="name" />
-                <DateField label="Действует с" source="start" />
-                <DateField label="Действует до" source="end" />
-                {canEdit ? <EditButton label={null} /> : <span></span>}
-            </CustomizableDatagrid>
+            {isXSmall ? (
+                <SimpleList
+                    primaryText={(record = {}) => `[${record.id}] ${_.get(record.parthner, ['full_name'], 'Партнер отсутствует')}`}
+                    secondaryText={(record) => 
+                        `Название: ${record.name}\n` +
+                        `Действует с: ${record.start}\n` +
+                        `Действует до: ${record.end}`
+                    }
+                    rowStyle={() => ({ whiteSpace: 'pre-line'})}
+                    linkType="show"  
+                />
+            ) : (
+                <CustomizableDatagrid rowClick="show" defaultColumns={defaultColumns} isRowSelectable={() => false}>
+                    <TextField label="id" source="id" />
+                    <TextField label="Партнер" source="parthner.full_name" />
+                    <TextField label="Название" source="name" />
+                    <DateField label="Действует с" source="start" />
+                    <DateField label="Действует до" source="end" />
+                    {canEdit ? <EditButton label={null} /> : <span></span>}
+                </CustomizableDatagrid>
+            ) 
+        }
         </List>
     );
 }
