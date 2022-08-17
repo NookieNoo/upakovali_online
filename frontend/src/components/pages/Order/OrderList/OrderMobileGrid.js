4ebbcd31bbf1f1689793de911d0664b614954afb
Andrey <ver.ver.sus.sus@mail.ru>
2022-08-17 17:34:14 +0300

    Карточка спика заказов

--- frontend/src/components/pages/Order/OrderList/OrderMobileGrid.js
+++ frontend/src/components/pages/Order/OrderList/OrderMobileGrid.js
@@ -4,14 +4,17 @@ import PropTypes from 'prop-types';
 import { List, ListItem, ListItemText } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 import { Link } from 'react-router-dom';
+import { TextField, BooleanField, DateField, ChipField } from 'react-admin';
 import { linkToRecord, sanitizeListRestProps, useListContext, RecordContextProvider } from 'ra-core';
 import { SimpleAccordionMemo } from '@app-universal';
+import { Card, CardContent } from '@material-ui/core';
+import Typography from '@material-ui/core/Typography';
 
 const useStyles = makeStyles(
     {
         tertiary: { float: 'right', opacity: 0.541176 },
     },
-    { name: 'RaSimpleList' }
+    { name: 'RaSimpleList' },
 );
 
 const OrderMobileGrid = (props) => {
@@ -27,6 +30,7 @@ const OrderMobileGrid = (props) => {
         rightIcon,
         secondaryText,
         tertiaryText,
+        useTranslate,
         rowStyle,
         ...rest
     } = props;
@@ -56,7 +60,60 @@ const OrderMobileGrid = (props) => {
                                         to={`${linkToRecord(basePath, id)}/show`}
                                         className={classes.link}
                                     >
-                                        <div>sdfdsf</div>
+                                        <Card>
+                                            <CardContent>
+                                                <Typography variant="body2">
+                                                   <span>
+                                                    Мастерская:&nbsp;</span>
+                                                    <TextField  label="Мастерская" source='workshop.address'/>
+                                                </Typography>
+                                                <Typography variant="body2">
+                                                    <span>
+
+                                                    Забор:&nbsp;</span><BooleanField label="Забор" source='is_pickupable'/>
+                                                </Typography>
+                                                <Typography variant="body2">
+                                                    <span>Доставка:&nbsp;</span>
+                                                    <BooleanField  label="Доставка" source='is_deliverable'/>
+                                                </Typography>
+                                                <Typography variant="body2">
+                                                    <span>Дата приема:&nbsp;</span>
+                                                    <DateField  label="Дата приема" source='receiving_date'/>
+                                                </Typography>
+                                                <Typography variant="body2">
+                                                    <span>Дата выдачи:&nbsp;</span>
+                                                    <DateField  label="Дата выдачи" source='issue_date'/>
+                                                </Typography>
+                                                <Typography variant="body2">
+                                                    <span>Комментарий:&nbsp;</span>
+                                                    <TextField  label="Комментарий" source='comment'/>
+                                                </Typography>
+                                                <Typography variant="body2">
+                                                    <span>Курьер приема:&nbsp;</span>
+                                                    <TextField  label="Курьер приема" source='courier_receiver.full_name'/>
+                                                </Typography>
+                                                <Typography variant="body2">
+                                                    <span>Курьер выдачи:&nbsp;</span>
+                                                    <TextField  label="Курьер выдачи" source='courier_issuer.full_name'/>
+                                                </Typography>
+                                                <Typography variant="body2">
+                                                    <span>Оплачено:&nbsp;</span>
+                                                    <BooleanField label="Оплачено" source="isPaid" />
+                                                </Typography>
+                                                <Typography variant="body2">
+                                                    <span>Мастер:&nbsp;</span>
+                                                    <TextField label="Мастер" source="master.full_name" />
+                                                </Typography>
+                                                <Typography variant="body2">
+                                                    <span>Получатель:&nbsp;</span>
+                                                    <TextField label="Получатель" source="receiver.full_name" />
+                                                </Typography>
+                                                <Typography variant="body2">
+                                                    <span>Статус:&nbsp;</span>
+                                                    <ChipField label="Статус" source="order_status.name" />
+                                                </Typography>
+                                            </CardContent>
+                                        </Card>
                                     </ListItem>
                                 </SimpleAccordionMemo>
                             </li>
